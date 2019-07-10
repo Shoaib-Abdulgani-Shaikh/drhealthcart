7990437e4b4b2a36cc3cabe4d13a3b411ff1f570
Dhanesh <dhaneshpawar.pro@gmail.com>
2019-07-10 17:20:52 +0530

    speed test

--- public/myac.js
+++ public/myac.js
@@ -4,7 +4,7 @@
           {
               scrollTop: $("#icon_telephone").offset().top
           },
-           100);
+           2000);
   }
 
 function myfunction() 
@@ -94,7 +94,7 @@ $('html, body').animate(
   {
       scrollTop: $("#activities").offset().top
   },
-   100);
+   2500);
 
 
         }})
