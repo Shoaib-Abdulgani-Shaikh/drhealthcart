9f831209e9133dcf03650cccaff0d21d5397c074
Sarang Barshikar <paramayurved@gmail.com>
2019-07-25 13:12:48 +0530

    db id saved to connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,3 +2,6 @@ const mongoose = require('mongoose');
 
 var db = mongoose.connect("mongodb://test:test123@ds261626.mlab.com:61626/drhealthcart");
 mongoose.connection.once('open',()=>{console.log();console.log('-- Connected to Dr. Healthcart Database--');console.log();}).on('error',(error)=>{console.log('continue error = ', error);});
+
+
+//mongodb+srv://test:test123@cluster0-o4zcv.mongodb.net/test?retryWrites=true&w=majority
